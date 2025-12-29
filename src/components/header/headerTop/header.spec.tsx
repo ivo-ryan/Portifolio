404792df141211cbe1b0241bac3886e66ee9fcfe
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-12-28 23:05:11 -0300

    update

---
+++ src/components/header/headerTop/header.spec.tsx
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Header from ".";
+
+
+describe("Header Top Component", () => {
+  it("Deve renderizar logo e menu", () => {
+        render(<Header />);
+        const title = screen.getByRole("heading", { name: /IR\.DEV/i });
+        const button = screen.getByLabelText("Abrir menu");
+        const homeLink = screen.getByRole("link", { name: /home/i });
+        const projetosLink = screen.getByRole("link", { name: /projetos/i });
+        const aboutLink = screen.getByRole("link", { name: /about/i });
+        
+        expect(homeLink).toBeInTheDocument();
+        expect(projetosLink).toBeInTheDocument();
+        expect(aboutLink).toBeInTheDocument();
+
+        expect(button).toBeInTheDocument();
+        expect(title).toBeInTheDocument();
+  });
+
+  it("O menu deve iniciar fechado e quando clicado deve abrir", async () => {
+        render(<Header />);
+
+        const user = userEvent.setup();
+
+        const nav = screen.getByRole("navigation");
+        const button = screen.getByLabelText("Abrir menu");
+        expect(nav).not.toBeVisible();
+    //   erro bem aqui o tobeVisible não funciona pois o nav não fica com o display none
+        await user.click(button);
+
+        expect(nav).toBeVisible();
+    });
+});
